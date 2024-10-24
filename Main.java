4fb6e2ad0e4183137999a2cb091afa3308ac587b
ohnoruu <rubydao007@gmail.com>
2024-10-24 11:22:26 -0700

    frhegutrgt

--- Main.java
+++ Main.java
@@ -4,6 +4,28 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+    Scanner sc = new Scanner(System.in);
+    // Problem 1
+    System.out.println("Please enter a double: ");
+    double num1 = sc.nextDouble();
+    if (num1 == 12.345){
+      System.out.println("YES");
+    }
+
+    //Problem 2 
+    System.out.println("Please enter an integer: ");
+    int num2 = sc.nextInt();
+    if (num2 == 48){
+      System.out.println("YES");
+    }
+
+    //Problem 3
+    System.out.println("Please enter two integers: ");
+    int num3 = sc.nextInt();
+    int num4 = sc.nextInt();
+    if ((2*num3)==num4){
+      
+    }
+    }
   }
 }
